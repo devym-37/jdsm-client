8bceb9551f519dad478eefc332cd1664b5c4e9fe
YoungMin Lee <miniyoung37@gmail.com>
2020-09-04 00:45:46 +0900

    add rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 import lessonReducer from "./lessonReducer";
+import coachReducer from "./coachReducer";
 
 const rootReducer = combineReducers({
   userReducer,
   lessonReducer,
+  coachReducer,
 });
 
 export default rootReducer;
