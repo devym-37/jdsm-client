49b22ecba717f04ecd8639015d6426e7acf060dc
YoungMin Lee <miniyoung37@gmail.com>
2020-09-04 00:48:42 +0900

    add Coach component

--- src/routes/Coach/CoachContainer.js
+++ src/routes/Coach/CoachContainer.js
@@ -1,30 +1,81 @@
 import React from "react";
 import { connect } from "react-redux";
 import CoachPresenter from "./CoachPresenter";
+import { addCoachProfile } from "../../redux/actions/coachActions";
 
+import { notification } from "antd";
 class CoachContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      coachForm: {
+        name: "",
+        age: "",
+        contact: "",
+      },
+    };
   }
 
+  handleChange = (e) => {
+    const { coachForm } = this.state;
+    const value = e.target.value;
+    const inputName = e.target.name;
+    this.setState({
+      coachForm: {
+        ...coachForm,
+        [inputName]: value,
+      },
+    });
+  };
+
+  handleNotification = () => {
+    notification.open({
+      message: "알림",
+      description: "빈칸에 내용을 입력하세요",
+    });
+  };
+  handleSubmit = () => {
+    const { coachForm } = this.state;
+    const { handleNotification } = this;
+    const { handleAddCoach, history } = this.props;
+
+    let submit = false;
+
+    for (const key in coachForm) {
+      if (coachForm[key] === "" && key !== "lesson") {
+        handleNotification();
+        break;
+      } else {
+        submit = true;
+      }
+    }
+    if (submit) {
+      handleAddCoach(coachForm);
+      history.push("/");
+    }
+  };
+
   render() {
-    return <CoachPresenter />;
+    const { coachForm } = this.state;
+    const { handleChange, handleSubmit } = this;
+    return (
+      <CoachPresenter
+        coachForm={coachForm}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+    );
   }
 }
 
-// const mapStateToProps = (state) => {
-//   return {
-//     player: state.userReducer.playerName,
-//   };
-// };
+const mapStateToProps = (state) => {
+  return {};
+};
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     handleReduxPlayer: (payload) => dispatch(playerSearch(payload)),
-//   };
-// };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleAddCoach: (payload) => dispatch(addCoachProfile(payload)),
+  };
+};
 
-// export default connect(mapStateToProps, mapDispatchToProps)(CoachContainer);
-
-export default CoachContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(CoachContainer);
--- src/routes/Coach/CoachPresenter.js
+++ src/routes/Coach/CoachPresenter.js
@@ -3,23 +3,29 @@ import styled from "styled-components";
 import Helmet from "react-helmet";
 import { Link } from "react-router-dom";
 
+import { Layout, Breadcrumb, Input, Select, Button } from "antd";
+
+const { Content } = Layout;
+const { Option } = Select;
+
 const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: #1ea1f7;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 18px;
 `;
 
 const Div = styled.div`
-  margin-bottom: 10px;
+  justify-content: flex-end;
+  display: flex;
+  margin-right: 30px;
+  width: 100px;
 `;
 
-const Input = styled.input`
-  width: 625px;
-  height: 50px;
+const Inputs = styled(Input)`
+  width: 300px;
+  height: 40px;
   padding: 15px 150px 18px 17px;
   line-height: 17px;
   font-size: 14px;
@@ -27,40 +33,76 @@ const Input = styled.input`
   border: none;
   border-radius: 0.4rem;
   transition: box-shadow 300ms;
-  &:focus {
-    outline: none;
-    box-shadow: 0.1rem 0.1rem 1rem #5e35b1;
-  }
 `;
 
-const IButton = styled.img`
-  padding: 10px;
-  justify-content: center;
-  align-items: center;
-  &:hover {
-    cursor: pointer;
-    box-shadow: 0.1rem 0.1rem 1rem #5e35b1;
-  }
-`;
-
-const Button = styled.button`
-  position: absolute;
-  height: 50px;
-  margin: 0;
-  margin-left: 10px;
-  padding: 0;
+const Selects = styled(Select)`
+  width: 300px;
+  font-size: 14px;
+  color: #9b9b9b;
   border: none;
-  outline: none;
-  border-radius: 5px;
+  border-radius: 0.4rem;
 `;
 
-const CoachPresenter = () => (
+const ButtonContainer = styled.div`
+  display: flex;
+  width: 500px;
+  justify-content: flex-end;
+  margin-top: 15px;
+`;
+
+const IButton = styled(Button)`
+  border-radius: 0.4rem;
+`;
+
+const CoachPresenter = ({ coachForm, handleChange, handleSubmit }) => (
   <>
     <Helmet>
-      <title>Coach Page</title>
+      <title>코치 등록</title>
     </Helmet>
 
-    <Container>Coach Page</Container>
+    <Content style={{ margin: "0 16px" }}>
+      <Breadcrumb style={{ margin: "30px 0" }}>
+        <Breadcrumb.Item
+          style={{
+            fontSize: "20px",
+            fontWeight: "bold",
+            marginLeft: "30px",
+          }}
+        >
+          코치 등록
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <Container>
+        <Div>이름</Div>
+        <Inputs
+          placeholder="내용을 입력해주세요"
+          name="name"
+          value={coachForm.name}
+          onChange={(e) => handleChange(e)}
+        />
+      </Container>
+      <Container>
+        <Div>나이</Div>
+        <Inputs
+          placeholder="내용을 입력해주세요"
+          name="age"
+          value={coachForm.age}
+          onChange={(e) => handleChange(e)}
+        />
+      </Container>
+      <Container>
+        <Div>연락처</Div>
+        <Inputs
+          placeholder="내용을 입력해주세요"
+          name="contact"
+          value={coachForm.contact}
+          onChange={(e) => handleChange(e)}
+        />
+      </Container>
+      <ButtonContainer>
+        <IButton onClick={handleSubmit}>등록</IButton>
+      </ButtonContainer>
+    </Content>
   </>
 );
 
