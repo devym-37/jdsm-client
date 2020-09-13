9ea2de931a47ce5c5a776799dba4c7917858defd
YoungMin Lee <miniyoung37@gmail.com>
2020-09-13 22:58:42 +0900

    add update & delete actions

--- src/redux/actions/lessonActions.js
+++ src/redux/actions/lessonActions.js
@@ -4,3 +4,13 @@ export const addLessonInfo = (lesson) => ({
   type: types.ADD_LESSON,
   lesson,
 });
+
+export const updateLessonInfo = (lesson) => ({
+  type: types.UPDATE_LESSON,
+  lesson,
+});
+
+export const deleteLessonInfo = (lesson) => ({
+  type: types.DELETE_LESSON,
+  lesson,
+});
