d24ddfde4e0bc307242d5d3e7956bc48d54c6c54
YoungMin Lee <miniyoung37@gmail.com>
2020-09-13 17:46:36 +0900

    add Actions

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -4,3 +4,13 @@ export const addUserProfile = (user) => ({
   type: types.ADD_USER,
   user,
 });
+
+export const deleteUserProfile = (user) => ({
+  type: types.DELETE_USER,
+  user,
+});
+
+export const updateUserProfile = (user) => ({
+  type: types.UPDATE_USER,
+  user,
+});
