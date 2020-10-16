205710a1e5942dc96765d44c74299510c57aaf24
YoungMin Lee <miniyoung37@gmail.com>
2020-10-17 00:25:18 +0900

    add memberThunk

--- src/redux/thunk/memberThnuk.js
+++ src/redux/thunk/memberThnuk.js
@@ -1,22 +1,33 @@
 import * as api from "../../api";
+import {
+  initMemberProfile,
+  addMemberProfile,
+  deleteMemberProfile,
+  updateMemberProfile,
+} from "../actions/memberActions";
 
 export const thunkGetMembers = () => {
   return async (dispatch, getState) => {
     const response = await api.member.getMembersInfo();
-    console.log("response :>> ", response);
+    const { data } = response.data;
+
+    if (response.status === 200 && data.length !== 0) {
+      dispatch(initMemberProfile(data));
+    }
   };
 };
 
 export const thunkPostMember = (payload) => {
   return async (dispatch, getState) => {
     const response = await api.member.registerMemberInfo(payload);
-    console.log("response :>> ", response);
+    console.log("post response", response);
+    return response.status;
   };
 };
 
 export const thunkGetMember = (payload) => {
   return async (dispatch, getState) => {
     const response = await api.member.getMemberInfo(payload);
-    console.log("response :>> ", response);
+    console.log("response 3:>> ", response);
   };
 };
