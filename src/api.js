9b92686bbf620559dbb433eb147067bbbe515f9c
YoungMin Lee <miniyoung37@gmail.com>
2020-10-22 00:40:58 +0900

    api error handle

--- src/api.js
+++ src/api.js
@@ -7,14 +7,30 @@ const api = axios.create({
 export const coach = {
   registerCoachInfo: (params) => api.post(`/coach`, params),
   updateCoachInfo: (params, id) => api.put(`/coach/${id}`, params),
-  getCoachesInfo: () => api.get(`/coach`),
-  getCoachInfo: (id) => api.get(`/coach/${id}`),
+  getCoachesInfo: () =>
+    api
+      .get(`/coach`)
+      .then((res) => res)
+      .catch((error) => console.log("err", error)),
+  getCoachInfo: (id) =>
+    api
+      .get(`/coach/${id}`)
+      .then((res) => res)
+      .catch((error) => console.log("err", error)),
 };
 
 export const member = {
   registerMemberInfo: (params) => api.post(`/members`, params),
-  getMembersInfo: () => api.get(`/members`),
-  getMemberInfo: (id) => api.get(`/member/${id}`),
+  getMembersInfo: () =>
+    api
+      .get(`/members`)
+      .then((res) => res)
+      .catch((error) => console.log("err", error)),
+  getMemberInfo: (id) =>
+    api
+      .get(`/member/${id}`)
+      .then((res) => res)
+      .catch((error) => console.log("err", error)),
 };
 
 export const lesson = {
@@ -24,9 +40,29 @@ export const lesson = {
   registerLessonMember: (params, id) =>
     api.post(`/lesson/${id}/members`, params),
   updateLessonInfo: (params, id) => api.put(`/lesson/${id}`, params),
-  getLessons: () => api.get(`/lesson`),
-  getLessonDay: (params) => api.get(`/lesson/days/${params}`),
-  getLessonInfo: (id) => api.get(`/lesson/${id}`),
-  getLessonCoaches: (id) => api.get(`/lesson/${id}/coaches`),
-  getLessonMembers: (id) => api.get(`/lesson/${id}/members`),
+  getLessons: () =>
+    api
+      .get(`/lesson`)
+      .then((res) => res)
+      .catch((error) => console.log("err", error)),
+  getLessonDay: (params) =>
+    api
+      .get(`/lesson/days/${params}`)
+      .then((res) => res)
+      .catch((error) => console.log("err", error)),
+  getLessonInfo: (id) =>
+    api
+      .get(`/lesson/${id}`)
+      .then((res) => res)
+      .catch((error) => console.log("err", error)),
+  getLessonCoaches: (id) =>
+    api
+      .get(`/lesson/${id}/coaches`)
+      .then((res) => res)
+      .catch((error) => console.log("err", error)),
+  getLessonMembers: (id) =>
+    api
+      .get(`/lesson/${id}/members`)
+      .then((res) => res)
+      .catch((error) => console.log("err", error)),
 };
